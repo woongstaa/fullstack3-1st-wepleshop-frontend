88adf02cecc482639292e454f7437f37e6fe96ed
woongstaa <jinung91@gmail.com>
2022-01-06 17:41:59 +0900

    Edit: css work

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // fetch(`${GET_PRODUCT_API}/5`).then(...).then(...);
-// const BASE_URL = 'http://localhost:8000';
-const BASE_URL = 'http://192.168.0.7:3000';
+const BASE_URL = 'http://localhost:8000';
+// const BASE_URL = 'http://192.168.0.7:3000';
 const GET_PRODUCT_API = `${BASE_URL}/products`;
 const GET_CATEGORY_API = `${BASE_URL}/category/sub`;
 const POST_SIGNIN_API = `${BASE_URL}/users/signin`;
