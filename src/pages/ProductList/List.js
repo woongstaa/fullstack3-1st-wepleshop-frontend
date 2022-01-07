927c9c058d88f555ad3f934198de7621712a802b
woongstaa <jinung91@gmail.com>
2022-01-07 11:12:12 +0900

    Edit: Update to CSS

---
+++ src/pages/ProductList/List.js
@@ -0,0 +1,17 @@
+import Footer from '../../components/footer/Footer';
+import Nav from '../../components/nav/Nav';
+import Top from '../../components/top/Top';
+import ProductList from './ProductList';
+
+const List = () => {
+  return (
+    <div>
+      <Top />
+      <Nav />
+      <ProductList />
+      <Footer />
+    </div>
+  );
+};
+
+export default List;
