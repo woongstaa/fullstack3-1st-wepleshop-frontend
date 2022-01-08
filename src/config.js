80fb12810b3f601afc9db4689aa61e223c60da45
박효상 <45405912+pd0203@users.noreply.github.com>
2022-01-08 19:01:10 +0900

    Edit: test

--- src/config.js
+++ src/config.js
@@ -2,14 +2,18 @@
 const BASE_URL = 'http://localhost:8000';
 // const BASE_URL = 'http://192.168.0.7:3000';
 const GET_PRODUCT_API = `${BASE_URL}/products`;
+const GET_LIST_API = `${BASE_URL}/products/list`;
 const GET_CATEGORY_API = `${BASE_URL}/category/sub`;
+const GET_DETAIL_API = `${BASE_URL}/products/details`;
 const POST_SIGNIN_API = `${BASE_URL}/users/signin`;
 const POST_SIGNUP_API = `${BASE_URL}/users/signup`;
 
 export {
   BASE_URL,
-  GET_CATEGORY_API,
   GET_PRODUCT_API,
+  GET_CATEGORY_API,
+  GET_LIST_API,
+  GET_DETAIL_API,
   POST_SIGNIN_API,
   POST_SIGNUP_API,
 };
