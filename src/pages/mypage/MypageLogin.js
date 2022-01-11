8eb9eaa8d28ad51431523ef0f293db8679cf46a7
Jongmin Choi <quark4904@gmail.com>
2022-01-11 23:16:44 +0900

    Add: mypage 로그인 token값을 이용한 사용자 이름 노출 구현

--- src/pages/mypage/MypageLogin.js
+++ src/pages/mypage/MypageLogin.js
@@ -1,10 +1,26 @@
+import { useState, useEffect } from 'react';
 import './Mypage.scss';
 
 function MypageLogin() {
+  const [userName, setUserName] = useState([]);
+  const userIdValue = sessionStorage.getItem('ID');
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/users/usernamefind`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json', mode: 'cors' },
+      body: JSON.stringify({
+        userId: userIdValue,
+      }),
+    })
+      .then(res => res.json())
+      .then(data => setUserName(data.name));
+  });
+
   return (
     <div className="myPageUserTop">
       <div className="myPageUser">
-        <div className="myPageUserText">반가워요</div>
+        <div className="myPageUserText">{userName}님, 반가워요.</div>
         <div className="myPageUserButtons">
           <div className="myPageUserButton">계정설정</div>
           <div className="myPageUserButton">로그아웃</div>
