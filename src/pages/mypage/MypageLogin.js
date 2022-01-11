e1c258397e31be9e6b2490f00a9ca26c8836954b
Jongmin Choi <quark4904@gmail.com>
2022-01-11 15:21:06 +0900

    Edit: mypage 로그인 상태 기능 구현중

--- src/pages/mypage/MypageLogin.js
+++ src/pages/mypage/MypageLogin.js
@@ -1,9 +1,29 @@
+import './Mypage.scss';
+
 function MypageLogin() {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="myPageUserTop">
+      <div className="myPageUser">
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+      </div>
     </div>
   );
 }
 
 export default MypageLogin;
+
+{
+  /* <div className="myPageTop">
+<p className="myPageTopText">크리에이터를 위한 위플샵을 만나보세요.</p>
+<div className="myPageTopButtons">
+  <Link to="/login">
+    <button className="myPageButton">로그인</button>
+  </Link>
+  <Link to="/login" state={{ isLoginSignUp: false }}>
+    <button className="myPageButton">회원가입</button>
+  </Link>
+</div>
+</div> */
+}
