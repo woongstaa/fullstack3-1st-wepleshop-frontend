9700f8e6f0160f19d630f27ef2979c45b4d0f05f
woongstaa <jinung91@gmail.com>
2022-01-13 21:17:56 +0900

    Edit: fix

--- src/pages/flowcard/FlowCard.js
+++ src/pages/flowcard/FlowCard.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react/cjs/react.development';
+import { useEffect, useState } from 'react';
 import { GET_FLOW_API } from '../../config';
 import './flowcard.scss';
 import Card from './card';
